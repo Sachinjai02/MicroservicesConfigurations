9849617dd9f760e486775cbc0fa3e6def0d19669
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-25 23:19:16 +0530

    Some variants of @Value 1. specifying default message 2. List 3. Map

--- src/main/java/com/study/microservices_configurations/controller/GreetingsController.java
+++ src/main/java/com/study/microservices_configurations/controller/GreetingsController.java
@@ -6,14 +6,26 @@ import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingsController {
 
-    @Value("${greeting.message}")
+    @Value("${greeting.message:defaultMessage}")
     private String greetingMessage;
 
+    @Value("Static message")
+    private String staticMessage;
+
+    @Value("${greeting.to.list}")
+    private List<String> members;
+
+    @Value("#{${dbParams}}")
+    private Map<String, String> dbParams;
     @GetMapping("/greeting")
     public String getGreetings() {
-        return this.greetingMessage;
+        return this.greetingMessage + " " + this.staticMessage
+                + " " + members + " " + dbParams;
     }
 }
