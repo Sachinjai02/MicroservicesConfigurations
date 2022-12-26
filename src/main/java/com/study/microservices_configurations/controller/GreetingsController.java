f709045aaf4776d55dd6ca0434c56a572628613f
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-26 12:25:21 +0530

    spring profiles - setting test profile as active Creating Bean only on 'test' profile

--- src/main/java/com/study/microservices_configurations/controller/GreetingsController.java
+++ src/main/java/com/study/microservices_configurations/controller/GreetingsController.java
@@ -3,6 +3,7 @@ package com.study.microservices_configurations.controller;
 import com.study.microservices_configurations.config.DBSettingsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@Profile("test")
 public class GreetingsController {
 
     @Value("${greeting.message:defaultMessage}")
